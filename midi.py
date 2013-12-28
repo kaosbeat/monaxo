7842c2bb456f7726745643f7dd6e73f94d895e59
kaosbeat <kasper.jordaens@gmail.com>
2013-12-28 23:04:46 +0000

    we have midi

--- midi.py
+++ midi.py
@@ -12,37 +12,32 @@ print "found!"
 m = Monome((host, port))
 m.start()
 
+#init the MIDI port
+midiout = rtmidi.MidiOut()
+available_ports = midiout.get_ports()
+print available_ports
+if available_ports:
+	midiout.open_port(1)
+else:
+	midiout.open_virtual_port("My virtual output")
 
-class Axo(object):
-    def __init__(self):
-    	#init the MIDI port
-		midiout = rtmidi.MidiOut()
-		available_ports = midiout.get_ports()
-		print available_ports
-		if available_ports:
-		    midiout.open_port(1)
-		else:
-		    midiout.open_virtual_port("My virtual output")
+def note_on(channel, note, velocity):
+	print channel
+	msg = [channel, note, velocity]
+	# msg = [0x99, 60, 112]
+	print msg
+	midiout.send_message(msg)
 
-    def note_on(self, channel, note, velocity):
-    	print self.channel
-    	self.msg = [channel, note, velocity]
-    	# msg = [0x99, 60, 112]
-    	print msg
-        self.midiout.send_message(msg)
+def note_off(channel, note):
+	midiout.send_message([channel, note, 0])
 
-    def note_on(self, channel, note):
-        self.midiout.send_message([channel, note, 0])
-
-a = Axo()
-# a.note_on(10, 60, 112)
 
 def keypressed(x, y, s):
-    m.led_set(x, y, s)
-    if s == 1:
-        a.note_on(60, 112)
-    else:
-        a.note_off(0x99, 60)
+	m.led_set(x, y, s)
+	if s == 1:
+		note_on(0x99,60, 112)
+	else:
+		note_off(0x99, 60)
 
 
 
@@ -50,10 +45,10 @@ m.grid_key = keypressed
 
 m.led_all(0)
 try:
-    while True:
-        for i in range(8):
-            time.sleep(1.0/20)
+	while True:
+		for i in range(8):
+			time.sleep(1.0/20)
 except KeyboardInterrupt:
-    # r.panic()
-    m.led_all(0)
-    m.close()
+	# r.panic()
+	m.led_all(0)
+	m.close()
